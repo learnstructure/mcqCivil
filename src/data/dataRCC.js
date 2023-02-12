8d4df12632f70c7bb33f1f00dc70c65181379f18
abinash <abinashmandal33486@gmail.com>
2023-02-12 22:55:47 +0545

    rcc data added

--- src/data/dataRCC.js
+++ src/data/dataRCC.js
@@ -1,19 +1,214 @@
 const dataRCC = [
     {
-        question: "A number of forces acting at a point will be in equilibrium if ",
-        optionA: "Their total sum is zero",
-        optionB: "Two resolved parts in two directions at right angles are equal",
-        optionC: "Sum of resolved parts in any two perpendicular directions are both zero",
-        optionD: "All of them are inclined equally", correct: "c"
+        question: "According to I.S. : 456, slabs which span in two directions with corners held down, are assumed to be divided in each direction into middle strips and edge strips such that the width of the middle strip, is",
+        optionA: "Half of the width of the slab",
+        optionB: "Two-third of the width of the slab",
+        optionC: "Three-fourth of the width of the slab",
+        optionD: "Four-fifth of the width of the slab",
+        correct: "c"
     },
     {
-        question: "A cable with a uniformly distributed load per horizontal metre run will take the following shape ",
-        optionA: "Straight line ", optionB: "Parabola", optionC: "Hyperbola", optionD: "Elliptical", correct: "b"
+        question: "The bond stress at a section can be reduced by",
+        optionA: "Decreasing the lever arm",
+        optionB: "Increasing the total perimeter of bars",
+        optionC: "Replacing larger bars by greater number of small bars",
+        optionD: "Replacing smaller bars by greater number of greater bars",
+        correct: "c"
     },
     {
-        question: "If a three hinged parabolic arch carries a uniformly distributed load on its entire span, every section of the arch resists-",
-        optionA: "Compressive force", optionB: "Tensile force", optionC: "Shear force", optionD: "Bending moment", correct: "a"
+        question: "The width of column strip in a flat slab as per IS 456 is kept",
+        optionA: "0.25 times the span length",
+        optionB: "0.25 times the diameter of the column",
+        optionC: "0.5 times the span length",
+        optionD: "0.5 times the diameter of the column",
+        correct: "a"
     },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' fixed at its ends, the maximum positive radial or circumferential moment at its center, is",
+        optionA: "3WR²/16",
+        optionB: "2WR²/16",
+        optionC: "WR²/16",
+        optionD: "None of above",
+        correct: "c"
+    },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' fixed at its ends, the maximum negative radial moment at its end, is",
+        optionA: "-3WR²/16",
+        optionB: "-2WR²/16",
+        optionC: "-WR²/16",
+        optionD: "None of above",
+        correct: "b"
+    },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' fixed at its ends, the maximum circumferential moment at its end, is",
+        optionA: "-3WR²/16",
+        optionB: "-2WR²/16",
+        optionC: "-WR²/16",
+        optionD: "0",
+        correct: "d"
+    },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' simply supported at its ends, the maximum positive radial or circumferential moment at its center, is",
+        optionA: "3WR²/16",
+        optionB: "2WR²/16",
+        optionC: "WR²/16",
+        optionD: "None of above",
+        correct: "a"
+    },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' simply supported at its ends, the maximum circumferential moment at its end, is",
+        optionA: "3WR²/16",
+        optionB: "2WR²/16",
+        optionC: "WR²/16",
+        optionD: "None of above",
+        correct: "b"
+    },
+    {
+        question: "If 'W' is the uniformly distributed load on a circular slab of radius 'R' simply supported at its ends, the maximum radial moment at its end, is",
+        optionA: "3WR²/16",
+        optionB: "2WR²/16",
+        optionC: "WR²/16",
+        optionD: "0",
+        correct: "d"
+    },
+    {
+        question: "The maximum ratio of span to depth of a slab simply supported and spanning in one direction is",
+        optionA: "35",
+        optionB: "25",
+        optionC: "30",
+        optionD: "20",
+        correct: "c"
+    },
+    {
+        question: "The maximum ratio of span to depth of a continous slab and spanning in one direction is",
+        optionA: "35",
+        optionB: "25",
+        optionC: "30",
+        optionD: "20",
+        correct: "a"
+    },
+    {
+        question: "The maximum ratio of span to depth of a slab simply supported (spanning in two direction) is",
+        optionA: "25",
+        optionB: "30",
+        optionC: "35",
+        optionD: "40",
+        correct: "c"
+    },
+    {
+        question: "The maximum ratio of span to depth of a continous slab (spanning in two direction) is",
+        optionA: "25",
+        optionB: "30",
+        optionC: "35",
+        optionD: "40",
+        correct: "d"
+    },
+    {
+        question: "If the width of the foundation for two equal columns is restricted, the shape of the footing generally adopted is",
+        optionA: "Square",
+        optionB: "Rectangular",
+        optionC: "Trapezoidal",
+        optionD: "Triangular",
+        correct: "b"
+    },
+    {
+        question: "The floor slab of a building is supported on reinforced cement floor beams. The ratio of the end and intermediate spans is 'generally' kept",
+        optionA: "0.7",
+        optionB: "0.8",
+        optionC: "0.9",
+        optionD: "0.6",
+        correct: "c"
+    },
+    {
+        question: "Pick up the correct statement from the following:",
+        optionA: "Lateral reinforcement in R.C.C. columns is provided to prevent the longitudinal reinforcement from buckling",
+        optionB: "Lateral reinforcement prevents the shearing of concrete on diagonal plane",
+        optionC: "Lateral reinforcement stops breaking away of concrete cover, due to buckling",
+        optionD: "All of the above",
+        correct: "d"
+    },
+    {
+        question: "Lapped splices in tensile reinforcement are generally not used for bars of size larger than",
+        optionA: "20 mm φ",
+        optionB: "25 mm φ",
+        optionC: "32 mm φ",
+        optionD: "36 mm φ",
+        correct: "d"
+    },
+    {
+        question: "Minimum spacing between horizontal parallel reinforcement of the same size should not be less than",
+        optionA: "One diameter",
+        optionB: "1.5 diameter",
+        optionC: "2 diameter",
+        optionD: "2.5 diameter",
+        correct: "a"
+    },
+    {
+        question: "Columns may be made of plain concrete if their unsupported lengths do not exceed their least lateral dimension",
+        optionA: "Two times",
+        optionB: "Three times",
+        optionC: "Four times",
+        optionD: "Five times",
+        correct: "b"
+    },
+    {
+        question: "A pre-stressed concrete member is preferred because",
+        optionA: "Its dimensions are not decided from the diagonal tensile stress",
+        optionB: "Large size of long beams carrying large shear force need not be adopted",
+        optionC: "Removal of cracks in the members due to shrinkage",
+        optionD: "All the above",
+        correct: "d"
+    },
+    {
+        question: "A pile of length 'L' carrying a uniformly distributed load 'W' per metre length is suspended at center, the maximum B.M. at the centre of the pile is",
+        optionA: "WL²/8 ",
+        optionB: "WL²/24 ",
+        optionC: "WL²/47 ",
+        optionD: "WL²/90 ",
+        correct: "a"
+    },
+    {
+        question: "A pile of length 'L' carrying a uniformly distributed load 'W' per metre length is suspended at two points, the maximum B.M. at the centre of the pile or at the points of suspension is",
+        optionA: "WL²/8 ",
+        optionB: "WL²/24 ",
+        optionC: "WL²/47 ",
+        optionD: "WL²/90 ",
+        correct: "c"
+    },
+    {
+        question: "A pile of length 'L' carrying a uniformly distributed load 'W' per metre length is suspended at three points, the maximum B.M. at the centre of the pile is",
+        optionA: "WL²/8 ",
+        optionB: "WL²/24 ",
+        optionC: "WL²/47 ",
+        optionD: "WL²/90 ",
+        correct: "d"
+    },
+    {
+        question: "The maximum permissible size of aggregates suitable for the construction of cement concrete floor is",
+        optionA: "7.5 mm",
+        optionB: "10 mm",
+        optionC: "12.5 mm",
+        optionD: "15 mm",
+        correct: "b"
+    },
+    {
+        question: "The minimum thickness of the cover at the end of a reinforcing bar should not be less than twice the diameter of the bar subject to a minimum of",
+        optionA: "10 mm",
+        optionB: "15 mm",
+        optionC: "20 mm",
+        optionD: "25 mm",
+        correct: "d"
+    },
+    /* {
+        question: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        optionD: "",
+        correct: ""
+    }, */
+
+
 
 ]
 //module.exports = dataRCC
--- src/data/dataStructure.js
+++ src/data/dataStructure.js
@@ -19,7 +19,151 @@ const dataStructure = [
         optionB: "Column analogy method ",
         optionC: "Moment distribution method ",
         optionD: "None of the above", correct: "b"
-    }
+    },
+    {
+        question: "If in a pin-jointed plane frame (m + r) > 2j, then the frame is (where m is number of members, r is reaction components and j is number of joints)",
+        optionA: "Stable and statically determinate",
+        optionB: "Stable and statically indeterminate",
+        optionC: "Unstable and statically determinate",
+        optionD: "Unstable and statically indeterminate",
+        correct: "b"
+    },
+    {
+        question: "When a uniformly distributed load, longer than the span of the girder, moves from left to right, then the maximum bending moment at mid-section of span occurs when the uniformly distributed load occupies",
+        optionA: "Less than the left half span",
+        optionB: "Whole of left half span",
+        optionC: "More than the left half span",
+        optionD: "Whole span",
+        correct: "d"
+    },
+    {
+        question: "For stable structures, one of the important properties of flexibility and stiffness matrices is that the elements on the main diagonal must be",
+        optionA: "negative",
+        optionB: "positive",
+        optionC: "zero",
+        optionD: "one",
+        correct: "b"
+    },
+    {
+        question: "When a load crosses a through type Pratt truss in the direction left to right, the nature of force in any diagonal member in the left half of the span would",
+        optionA: "Change from compression to tension",
+        optionB: "Change from tension to compression",
+        optionC: "Always be compression",
+        optionD: "Always be tension",
+        correct: "a"
+    },
+    {
+        question: "Which of the following is not a displacement method?",
+        optionA: "Equilibrium method",
+        optionB: "Column analogy method",
+        optionC: "Moment distribution method",
+        optionD: "Kani's method",
+        correct: "b"
+    },
+    {
+        question: "For a symmetrical two hinged parabolic arch, if one of the supports settles horizontally, then the horizontal thrust",
+        optionA: "is increased",
+        optionB: "is decreased",
+        optionC: "remains unchanged ",
+        optionD: "becomes zero ",
+        correct: "b"
+    },
+    {
+        question: "The deflection at any point of a plane frame can be obtained by applying a unit load at the joint in",
+        optionA: "Vertical direction",
+        optionB: "Horizontal direction",
+        optionC: "Inclined direction",
+        optionD: "The direction in which the deflection is required",
+        correct: "d"
+    },
+    {
+        question: "The principle of virtual work can be applied to elastic system by considering the virtual work of",
+        optionA: "Internal forces only",
+        optionB: "External forces only",
+        optionC: "Internal as well as external forces",
+        optionD: "Either internal or external forces",
+        correct: "c"
+    },
+    {
+        question: "If in a rigid-jointed space frame, (6m + r) < 6j, then the frame is",
+        optionA: "Unstable",
+        optionB: "Stable and statically determinate",
+        optionC: "Stable and statically indeterminate",
+        optionD: "None of the above",
+        correct: "a"
+    },
+    {
+        question: "The three moments equation is applicable only when",
+        optionA: "The beam is prismatic",
+        optionB: "There is no settlement of supports",
+        optionC: "There is no discontinuity such as hinges within the span",
+        optionD: "Both b and c",
+        correct: "c"
+    },
+    {
+        question: "Which of the following methods of structural analysis is a displacement method?",
+        optionA: "Method of consistent deformation",
+        optionB: "Column analogy method ",
+        optionC: "Three moment equation ",
+        optionD: "Moment distribution method",
+        correct: "d"
+    },
+    {
+        question: "The fixed support in a real beam becomes in the conjugate beam a",
+        optionA: "Roller support",
+        optionB: "Hinged support",
+        optionC: "Fixed support",
+        optionD: "Free end",
+        correct: "d"
+    },
+    {
+        question: "To generate the jth column of the flexibility matrix",
+        optionA: "A unit force is applied at coordinate j and the displacements are calculated at all coordinates",
+        optionB: "A unit displacement is applied at coordinate j and the forces are calculated at all coordinates",
+        optionC: "A unit force is applied at coordinate j and the forces are calculated at all coordinates",
+        optionD: "A unit displacement is applied at coordinate j and the displacements are calculated at all coordinates ",
+        correct: "a"
+    },
+    {
+        question: "A load W is moving from left to right on a simply supported beam of span L. The maximum bending moment at 0.4L from the left support is",
+        optionA: "0.16 WL",
+        optionB: "0.20 WL",
+        optionC: "0.24 WL",
+        optionD: "0.30 WL",
+        correct: "c"
+    },
+    {
+        question: "In the displacement method of structural analysis, the basic unknowns are",
+        optionA: "Displacements",
+        optionB: "Force",
+        optionC: "Displacements and forces",
+        optionD: "None of the above",
+        correct: "a"
+    },
+    {
+        question: "A rigid-jointed plane frame is stable and statically determinate if (Where m is number of members, r is reaction components and j is number of joints)",
+        optionA: "(m + r) = 2j",
+        optionB: "(m + r) = 3j",
+        optionC: "(3m + r) = 3j",
+        optionD: "(m + 3r) = 3j",
+        correct: "c"
+    },
+    {
+        question: "In moment distribution method, the sum of distribution factors of all the members meeting at any joint is always",
+        optionA: "0",
+        optionB: "less than 1",
+        optionC: "1",
+        optionD: "greater than 1",
+        correct: "c"
+    },
+    /* {
+        question: "",
+        optionA: "",
+        optionB: "",
+        optionC: "",
+        optionD: "",
+        correct: ""
+    }, */
 ]
 const Random = (data) => {
     const num = 2;
