d6cf1622527d0fdbc0649ab91a0cf3e86e3ed597
abinash <abinashmandal33486@gmail.com>
2023-05-20 19:59:55 +0545

    steel design v1

--- src/structure-tools/steel-design/steelColumnI/SteelColumnI.js
+++ src/structure-tools/steel-design/steelColumnI/SteelColumnI.js
@@ -1,29 +1,55 @@
 import React, { useState, createContext } from 'react'
 import { Helmet } from 'react-helmet'
-import { get_section_props } from './steelModule'
+import { get_section_props, buckling_curve, design_compression } from './steelModule'
 
 export const RccColumnParams = createContext()
 function SteelColumnI() {
-    const [result, setResult] = useState(null)
-    const [params, setParams] = useState({ D: 500, B: 300, l: 1.8, pu: 200, mux: 100, muy: 50, fy: 415, fck: 25, tf: 0, tw: 0 });
-    const [section, setSection] = useState("ISMB100")
+    const [result, setResult] = useState({ fcd_y: 0, fcd_z: 0 })
+    const [params, setParams] = useState({ pu: 200, mux: 100, muy: 50, fy: 250, tf: 16, tw: 8.9, h: 400, bf: 140, A: 7840, kly: 5000, klz: 5000, ry: 28.2, rz: 162, E: 200000 });
+    const [section, setSection] = useState({ section: "ISMB400", buckling_z: "a", buckling_y: 'b' })
+    const [connection, setConnection] = useState("rolled")
+    const [isOtherSelected, setIsOtherSelected] = useState(false);
     const handleSectionChange = (event) => {
-        const value = event.target.value
-        setSection(value)
-        const [tf, tw] = get_section_props(value)
-        setParams({ ...params, tf: tf, tw: tw });
+        const { name, value } = event.target
+
+        setSection((prevSection) => ({
+            ...prevSection,
+            [name]: value
+        }));
+
+        let section_props
+        if (value !== "Other") {
+            section_props = get_section_props(value)
+
+            setParams((prevParams) => ({
+                ...prevParams,
+                ...section_props
+            }))
+        }
+        setIsOtherSelected(value === "Other");
+        /* const [buckling_z, buckling_y] = buckling_curve(connection, params.h, params.bf, params.tf)
+        setSection({ ...section, buckling_z: buckling_z, buckling_y: buckling_y }) */
+        // console.log(section.section)
     };
+    const handleConnection = (event) => {
+        setConnection(event.target.value)
+        const [buckling_z, buckling_y] = buckling_curve(connection, params.h, params.bf, params.tf)
+        setSection({ ...section, buckling_z: buckling_z, buckling_y: buckling_y })
+
+    }
     const handleChange = (event) => {
         const value = parseFloat(event.target.value)
         const { name } = event.target;
         setParams({ ...params, [name]: value });
+
     };
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        /*  setResult(<RccColumnParams.Provider value={{ params }} >
-             <GetInteraction />
-         </RccColumnParams.Provider>) */
+        /* const Pd_z = design_compression(section.buckling_z, params.klz, params.A, params.rz, params.fy, params.E)
+        console.log(Pd_z) */
+        setResult({ ...result, fcd_z: design_compression(section.buckling_z, params.klz, params.A, params.rz, params.fy, params.E), fcd_y: design_compression(section.buckling_y, params.kly, params.A, params.ry, params.fy, params.E) })
+        //console.log(section.section)
 
     };
     return (
@@ -35,6 +61,11 @@ function SteelColumnI() {
             <h2>Steel Column Design (I-Section)</h2>
             <div className='calc-container'>
                 <form onSubmit={handleSubmit} className='params'>
+                    <label>
+                        f<sub>y</sub> :
+                        <input type="number" name="fy" value={params.fy} onChange={handleChange} min="5"
+                            step="5" className='input-number' /> N/mm²
+                    </label>
                     <label className='param-label'>Loadings (factored)</label>
                     <label>
                         P<sub>u</sub> :
@@ -53,40 +84,58 @@ function SteelColumnI() {
                     </label>
                     <label className='param-label'>Column Parameters</label>
                     <label >Choose a section:
-                        <select name="section" id="section" onChange={handleSectionChange}>
+                        <select name="section" id="section" defaultValue="ISMB400" onChange={handleSectionChange} className='dropdown-select'>
                             <option value="ISMB100">ISMB100</option>
                             <option value="ISMB150">ISMB150</option>
                             <option value="ISMB200">ISMB200</option>
                             <option value="ISMB250">ISMB250</option>
                             <option value="ISMB300">ISMB300</option>
+                            <option value="ISMB350">ISMB350</option>
+                            <option value="ISMB400">ISMB400</option>
                             <option value="Other">Other</option>
                         </select>
                     </label>
-                    <h3>Option selected is {section}</h3>
-                    <h3>tf is {params.tf}</h3>
-                    <h3>tw is {params.tw}</h3>
+                    <p>section is {section.section}</p>
                     <label>
-                        D (Major-x):
-                        <input type="number" name="D" value={params.D} onChange={handleChange} min="0.01"
-                            step="0.01" className='input-number' /> mm
+                        h (Overall depth):
+                        <input type="number" name="h" value={params.h} onChange={handleChange} min="10"
+                            step="5" className='input-number' disabled={!isOtherSelected} /> mm
                     </label>
                     <label>
-                        B (Minor-y):
-                        <input type="number" name="B" value={params.B} onChange={handleChange} min="0.01"
-                            step="0.01" className='input-number' /> mm
+                        b<sub>f</sub> (Flange width):
+                        <input type="number" name="bf" value={params.bf} onChange={handleChange} min="10"
+                            step="5" className='input-number' disabled={!isOtherSelected} /> mm
+                    </label>
+                    <label>
+                        t<sub>w</sub> (Web thickness):
+                        <input type="number" name="tw" value={params.tw} onChange={handleChange} min="1"
+                            step="0.1" className='input-number' disabled={!isOtherSelected} /> mm
+                    </label>
+                    <label>
+                        t<sub>f</sub> (Flange thickness):
+                        <input type="number" name="tf" value={params.tf} onChange={handleChange} min="1"
+                            step="0.1" className='input-number' disabled={!isOtherSelected} /> mm
                     </label>
 
-                    <label className='param-label'>Design Parameters</label>
+                    <label >Cross section elements:
+                        <select name="connection" onChange={handleConnection} disabled={!isOtherSelected} className='dropdown-select'>
+                            <option value="rolled">Rolled</option>
+                            <option value="welded">Welded</option>
+                        </select>
+                    </label>
+
+                    <label className='param-label'>Effective length Parameters</label>
                     <label>
-                        f<sub>y</sub> :
-                        <input type="number" name="fy" value={params.fy} onChange={handleChange} min="5"
-                            step="5" className='input-number' /> N/mm²
+                        K L<sub>y</sub> (about y-y):
+                        <input type="number" name="kly" value={params.kly} onChange={handleChange} min="10"
+                            step="10" className='input-number' /> mm
                     </label>
                     <label>
-                        f<sub>ck</sub> :
-                        <input type="number" name="fck" value={params.fck} onChange={handleChange} min="1"
-                            step="1" className='input-number' /> N/mm²
+                        K L<sub>z</sub> (about z-z):
+                        <input type="number" name="klz" value={params.klz} onChange={handleChange} min="10"
+                            step="10" className='input-number' /> mm
                     </label>
+
 
                     {/* <img src='/images/column.PNG' alt="column design" style={{ maxWidth: '100%' }} /> */}
 
@@ -95,7 +144,8 @@ function SteelColumnI() {
             </div>
             <h3>Results</h3>
             <div className='calc-container'>
-                {result}
+                <p> f<sub>cd,z</sub> = {result.fcd_z.toFixed(2)} N/mm²</p>
+                <p> f<sub>cd,y</sub> = {result.fcd_y.toFixed(2)} N/mm²</p>
             </div>
 
         </main>
--- src/structure-tools/steel-design/steelColumnI/steelModule.js
+++ src/structure-tools/steel-design/steelColumnI/steelModule.js
@@ -1,16 +1,185 @@
-export function get_section_props(section) {
-    var tf, tw
+/* export function get_section_props(section) {
+    var A, tf, tw, h, bf
     switch (section) {
 
         case 'ISMB100':
-            tf = 10
+            A = 1140
+            h = 100
+            bf = 50
+            tw = 4.7
+            tf = 7
+            break;
+        case 'ISMB150':
+            A = 1910
+            h = 150
+            bf = 75
             tw = 8
+            tf = 8
+            break;
+        case 'ISMB200':
+            A = 3080
+            h = 200
+            bf = 100
+            tw = 4.7
+            tf = 7
+            break;
+        case 'ISMB250':
+            A = 1140
+            h = 100
+            bf = 50
+            tw = 5.7
+            tf = 10
+            break;
+        case 'ISMB300':
+            A = 5860
+            h = 300
+            bf = 140
+            tw = 7.7
+            tf = 13.1
+            break;
+        case 'ISMB350':
+            A = 6670
+            h = 350
+            bf = 140
+            tw = 8.1
+            tf = 14.2
             break;
 
-        default:
-            tf = 12
-            tw = 7
+        default:    //for ISMB400
+            A = 7840
+            h = 400
+            bf = 140
+            tw = 8.9
+            tf = 16
             break;
     }
-    return [tf, tw]
+    return [A, h, bf, tw, tf]
+} */
+
+export function get_section_props(section) {
+    let props = {};
+    switch (section) {
+        case 'ISMB100':
+            props = {
+                A: 1140,
+                h: 100,
+                bf: 50,
+                tw: 4.7,
+                tf: 7,
+                rz: 40,
+                ry: 10.5
+            };
+            break;
+        case 'ISMB150':
+            props = {
+                A: 1910,
+                h: 150,
+                bf: 75,
+                tw: 8,
+                tf: 8,
+                rz: 61.3,
+                ry: 15.7
+            };
+            break;
+        case 'ISMB200':
+            props = {
+                A: 3080,
+                h: 200,
+                bf: 100,
+                tw: 4.7,
+                tf: 7,
+                rz: 82.9,
+                ry: 21.2
+            };
+            break;
+        case 'ISMB250':
+            props = {
+                A: 1140,
+                h: 100,
+                bf: 50,
+                tw: 5.7,
+                tf: 10,
+                rz: 104,
+                ry: 26.5
+            };
+            break;
+        case 'ISMB300':
+            props = {
+                A: 5860,
+                h: 300,
+                bf: 140,
+                tw: 7.7,
+                tf: 13.1,
+                rz: 124,
+                ry: 28.6
+            };
+            break;
+        case 'ISMB350':
+            props = {
+                A: 6670,
+                h: 350,
+                bf: 140,
+                tw: 8.1,
+                tf: 14.2,
+                rz: 143,
+                ry: 28.4
+            };
+            break;
+        default: //for ISMB400
+            props = {
+                A: 7840,
+                h: 400,
+                bf: 140,
+                tw: 8.9,
+                tf: 16,
+                rz: 162,
+                ry: 28.2
+            };
+            break;
+    }
+    return props;
+}
+
+export function buckling_curve(connection, h, bf, tf) {
+    //let z_z, y_y
+    if (connection === "rolled") {
+        if (h / bf > 1.2) {
+            return tf <= 40 ? ['a', 'b'] : ['b', 'c']
+        } else {
+            return tf <= 100 ? ['b', 'c'] : ['d', 'd']
+        }
+    } else {
+        /* if (tf <= 40) {
+            return ['b', 'c']   //for major and minor axis
+        } else {
+            return ['c', 'd']
+        } */
+        return tf <= 40 ? ['b', 'c'] : ['c', 'd'] //for major and minor axis
+    }
+
+}
+
+function imperfection_factor(buckling_class) {
+    switch (buckling_class) {
+        case 'a':
+            return 0.21
+        case 'b':
+            return 0.34
+        case 'c':
+            return 0.49
+        default:
+            return 0.76
+    }
+}
+export function design_compression(buckling_class, l_eff, A, r, fy, E) {
+    const alpha = imperfection_factor(buckling_class)
+    const slenderness = l_eff / r
+    const fcc = Math.pow(Math.PI, 2) * E / Math.pow(slenderness, 2)
+    const lambda = Math.pow(fy / fcc, 0.5)
+    const phi = 0.5 * (1 + alpha * (lambda - 0.2) + Math.pow(lambda, 2))
+    let fcd = fy / (phi + Math.pow(phi * phi - lambda * lambda, 0.5))
+    fcd = fcd > fy ? fy / 1.1 : fcd / 1.1
+    console.log(fcd)
+    console.log(r)
+    return fcd
 }
