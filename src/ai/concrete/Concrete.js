e60e98f7c3b666a0fc4335f1a68310d5e18710c8
abinash <abinashmandal33486@gmail.com>
2024-10-14 21:01:20 +0545

    SVM and LR also added

--- src/ai/concrete/Concrete.js
+++ src/ai/concrete/Concrete.js
@@ -28,8 +28,7 @@ const Concrete = () => {
                 throw new Error('Network response was not ok');
             }
             const data = await response.json();
-            const strength = parseFloat(data.prediction).toFixed(2); // Convert to float, then format to 2 decimal places
-            setPrediction(strength);
+            setPrediction(data);
         } catch (error) {
             console.error('Error fetching the prediction:', error);
             // Optionally, set an error state here to display to the user
@@ -43,54 +42,76 @@ const Concrete = () => {
                 <title>Compressive strength</title>
                 <meta name="description" content="Predict compressive strength of concrete using advanced machine learning techniques" />
             </Helmet>
-            <h2>Predict Concrete Compressive Strength</h2>
-            <div className='params3'>
-
-                <label>
-                    Cement:
-                    <input type="number" name="cem" value={params.cem} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Water:
-                    <input type="number" name="water" value={params.water} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Coarse aggregate:
-                    <input type="number" name="ca" value={params.ca} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Fine aggregate:
-                    <input type="number" name="fa" value={params.fa} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Age:
-                    <input type="number" name="age" value={params.age} onChange={handleChange} min="0"
-                        className='input-number' /> days
-                </label>
-                <label>
-                    Superplasticizer:
-                    <input type="number" name="sp" value={params.sp} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Flyash:
-                    <input type="number" name="flyash" value={params.flyash} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
-                <label>
-                    Blast furnace slag:
-                    <input type="number" name="bfs" value={params.bfs} onChange={handleChange} min="0"
-                        className='input-number' /> kg/m³
-                </label>
+            <h2>Predict Compressive Strength of Concrete</h2>
+            <div className='params3 prediction-box '>
+                <div >
+                    <label >
+                        Cement:
+                        <input type="number" name="cem" value={params.cem} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div >
+                    <label >
+                        Water:
+                        <input type="number" name="water" value={params.water} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Coarse aggregate:
+                        <input type="number" name="ca" value={params.ca} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div >
+                    <label>
+                        Fine aggregate:
+                        <input type="number" name="fa" value={params.fa} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div >
+                    <label>
+                        Age:
+                        <input type="number" name="age" value={params.age} onChange={handleChange} min="0"
+                            className='input-number' /> days
+                    </label>
+                </div>
+                <div >
+                    <label>
+                        Superplasticizer:
+                        <input type="number" name="sp" value={params.sp} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div >
+                    <label>
+                        Flyash:
+                        <input type="number" name="flyash" value={params.flyash} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
+                <div >
+                    <label>
+                        Blast furnace slag:
+                        <input type="number" name="bfs" value={params.bfs} onChange={handleChange} min="0"
+                            className='input-number' /> kg/m³
+                    </label>
+                </div>
 
             </div>
 
             <button onClick={handlePredict} className='button'>Predict</button>
-            {prediction && <p>Predicted Strength: {prediction} MPa (Gradient boosting model)</p>}
+            {prediction && (
+                <div className="prediction-box">
+                    <p className="prediction-title">Predictions based on various machine learning models</p>
+                    <p className="prediction-text">Gradient Boosting: {parseFloat(prediction.gbr).toFixed(2)} MPa</p>
+                    <p className="prediction-text">Support vector machine: {parseFloat(prediction.svr).toFixed(2)} MPa</p>
+                    <p className="prediction-text">Linear Regression: {parseFloat(prediction.linear).toFixed(2)} MPa</p>
+                </div>
+            )}
         </main>
     );
 };
